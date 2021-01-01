57c538c2c6b7079c2296ce2a9430626151bc886a
Jaf <jaffreyrodriguez42@gmail.com>
2021-01-01 15:38:01 +0800

    Give your Blog a database

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require("mongoose");
+
+mongoose.connect('mongodb://localhost:27017/blogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const blogSchema = ({
+	title: String,
+	body: String
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +26,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
-
 app.get('/', function(req, res){
-	res.render('home', {homeStartingContent: homeStartingContent, posts : posts});
+	Blog.find({}, function(err, posts){
+		if(err){
+			console.log(err);
+		}else{
+			res.render('home', {homeStartingContent: homeStartingContent, posts : posts});
+		}
+	});
 });
 
 app.get('/about', function(req, res){
@@ -35,24 +49,25 @@ app.get('/compose', function(req, res){
 });
 
 app.post('/compose', function(req, res){
+
 	let postTitle = req.body.postTitle;
 	let postBody = req.body.postBody;
 
-	let post = {
-		postTitle: postTitle,
-		postBody: postBody
-	};
-	posts.push(post);
-	res.redirect("/");
+	let blog = new Blog({
+		title: postTitle,
+		body: postBody
+	});
+
+	blog.save();
 });
 
-app.get('/posts/:postName', function(req, res){
-	let requestedTitle = req.params.postName;
-	requestedTitle = _.lowerCase(requestedTitle);
-	posts.forEach(function(post){
-		if(_.lowerCase(post.postTitle) === requestedTitle){
-			// console.log("Match Found!");
-			res.render('post', {postTitle: post.postTitle, postBody: post.postBody});
+app.get('/posts/:postId', function(req, res){
+	let postId = req.params.postId;
+	Blog.findOne({_id: postId}, function(err, foundPost){
+		if(err){
+			console.log(err);
+		}else{
+			res.render('post', {postTitle: foundPost.title, postBody: foundPost.body});
 		}
 	});
 });
